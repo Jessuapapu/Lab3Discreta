899578eb518eec1ad988cc8be5e23d6cfbe67b78
Jessua Solis <147453642+Jessuapapu@users.noreply.github.com>
2024-10-07 23:36:40 -0600

    Terminar logica de sumas y esas cosas

---
+++ Declaraciones.py
@@ -0,0 +1,12 @@
+
+# Declaraciones de variables globales
+ConjuntoA = []
+ConjuntoB = []
+ConjuntoC = []
+ConjuntoD = []
+ConjuntoU = []
+
+
+con1 = []
+con2 = []
+
---
+++ Interfaz.py
@@ -0,0 +1,9 @@
+import Conjuntos as CJ
+import tkinter as tk
+import CompTkinter as CT
+
+
+AppMain.master.title("Laboratorio 3 - Matematicas discretas")
+AppMain.master .maxsize(500,500)
+
+AppMain.mainloop()
